e0dc8194029189df8ca8e894078fef6a8e2afc0f
nancyhenry89 <nancyhenry89@gmail.com>
2018-05-16 15:43:43 +0200

    New

--- react_spa/src/Home.js
+++ react_spa/src/Home.js
@@ -1,18 +1,128 @@
 import React, { Component } from "react";
+import "./home.css";
+import sec1 from './assets/images/sec1.svg'
+import sec2 from './assets/images/sec2.svg'
+import sec3 from './assets/images/sec3.svg'
+import sec4 from './assets/images/sec4.svg'
+import sec5 from './assets/images/sec5.svg'
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
-
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+      <div id="home">
+        <div className="main-banner">
+          <div className="inner-cont">
+            <div className="slogan">
+              Watch Your Website Build Itself
+            </div>
+            <p>Create a Website in 3 steps</p>
+            <div className="banner-actions">
+              <button>Get Started!</button>
+              <button>Watch a video demo</button>
+            </div>
+          </div>
+        </div>
+        <div className='what'>
+          <div className="inner-cont">
+            <div className="row">
+              <h2 className="section-title">What you get</h2>
+              <div className="desc">We have more than</div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <div className="head">
+                  <span className="icon"></span>
+                </div>
+                <div className="body">
+                  <div className="title">Unique designs</div>
+                  <div className="details">Create separate sections for different aspects of your business</div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="head">
+                  <span className="icon"></span>
+                </div>
+                <div className="body">
+                  <div className="title">Unique designs</div>
+                  <div className="details">Create separate sections for different aspects of your business</div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="head">
+                  <span className="icon"></span>
+                </div>
+                <div className="body">
+                  <div className="title">Unique designs</div>
+                  <div className="details">Create separate sections for different aspects of your business</div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="head">
+                  <span className="icon"></span>
+                </div>
+                <div className="body">
+                  <div className="title">Unique designs</div>
+                  <div className="details">Create separate sections for different aspects of your business</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="sec1 home-section img-first">
+          <div className="inner-cont">
+            <div className="img-cont">
+              <img src={sec1}/>
+            </div>
+            <div className="details-cont">
+              <div className="title">Pre-designed sections to play with</div>
+              <div className="desc">Any edits to your website are automatically sythesized on desktop, tablet and mobile devices.</div>
+            </div>
+          </div>
+        </div>
+        <div className="sec2 home-section img-last">
+          <div className="inner-cont">
+            <div className="details-cont">
+              <div className="title">Pre-designed sections to play with</div>
+              <div className="desc">Any edits to your website are automatically sythesized on desktop, tablet and mobile devices.</div>
+            </div>
+            <div className="img-cont">
+              <img src={sec2}/>
+            </div>
+          </div>
+          </div>
+          <div className="sec3 home-section img-first">
+            <div className="inner-cont">
+              <div className="img-cont">
+                <img src={sec3}/>
+              </div>
+              <div className="details-cont">
+                <div className="title">Pre-designed sections to play with</div>
+                <div className="desc">Any edits to your website are automatically sythesized on desktop, tablet and mobile devices.</div>
+              </div>
+            </div>
+          </div>
+          <div className="sec4 home-section img-last">
+            <div className="inner-cont">
+              <div className="details-cont">
+                <div className="title">Pre-designed sections to play with</div>
+                <div className="desc">Any edits to your website are automatically sythesized on desktop, tablet and mobile devices.</div>
+              </div>
+              <div className="img-cont">
+                <img src={sec4}/>
+              </div>
+            </div>
+            <div className="sec5 home-section img-first">
+              <div className="inner-cont">
+                <div className="img-cont">
+                  <img src={sec5}/>
+                </div>
+                <div className="details-cont">
+                  <div className="title">Pre-designed sections to play with</div>
+                  <div className="desc">Any edits to your website are automatically sythesized on desktop, tablet and mobile devices.</div>
+                </div>
+              </div>
+            </div>
+        </div>
       </div>
     );
   }
--- react_spa/src/Main.js
+++ react_spa/src/Main.js
@@ -7,18 +7,33 @@ import {
 import Home from "./Home";
 import Stuff from "./Stuff";
 import Contact from "./Contact";
-
+import logo from './assets/images/logo.png';
+import flag from './assets/images/arabic-flag.png'
 class Main extends Component {
   render() {
     return (
       <HashRouter>
         <div>
-          <h1>Simple SPA</h1>
+        <div className="header-cont">
+        <div className="inner-cont">
+          <h1 className="logo">
+            <img alrt="logo" src={logo} />
+          </h1>
           <ul className="header">
             <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink to="/stuff">Stuff</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
+            <li><NavLink to="/stuff">Features</NavLink></li>
+            <li><NavLink to="/contact">Pricing</NavLink></li>
           </ul>
+          <div className="header-opo">
+            <div className="account">Aleardy Have an Account?</div>
+            <button>Build website now</button>
+            <div className="lang-switcher">
+              <div className="flag"><img src={flag}/></div>
+              <div className="popup"></div>
+            </div>
+          </div>
+          </div>
+          </div>
           <div className="content">
             <Route exact to="/" component={Home}/>
             <Route path="/stuff" component={Stuff}/>
